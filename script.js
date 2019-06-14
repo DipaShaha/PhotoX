a4f51ab8b8e32be4ea05e011daa4cbe7dc7f89c3
DipaShaha <dipashaha92@gmail.com>
2019-06-14 20:51:44 +0600

    commit|completed

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
-  
-  //colaapse button
- 
   $('.nav-button').click(function() {
     $('.nav-button').toggleClass('change');
   });
 
-  //navbar change
   $(window).scroll(function() {
     let position = $(this).scrollTop();
     if(position >= 200) {
@@ -16,8 +12,6 @@ $(document).ready(function() {
     }
   });
 
-
-  //mission section animation
   $(window).scroll(function() {
     let position = $(this).scrollTop();
     if(position >= 650) {
@@ -29,26 +23,20 @@ $(document).ready(function() {
     }
   });
 
-
- // gallery section
- $('.gallery-list-item').click(function(){
-    let value=$(this).attr('data-filter');
-    if(value==='all'){
+  $('.gallery-list-item').click(function() {
+    let value = $(this).attr('data-filter');
+    if(value === 'all') {
       $('.filter').show(300);
-    }else{
-      $('.filter').not('.'+value).hide(300);
-      $('.filter').filter('.'+value).show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
     }
- })
+  });
 
+  $('.gallery-list-item').click(function() {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+  });
 
-$('.gallery-list-item').click(function(){
-  $(this).addClass('active-item').siblings().removeClass('active-item');
-})
-
-
-// pricing animation
-  //mission section animation
   $(window).scroll(function() {
     let position = $(this).scrollTop();
     if(position >= 4300) {
@@ -56,14 +44,11 @@ $('.gallery-list-item').click(function(){
       $('.card-2').addClass('moveFromBottom');
       $('.card-3').addClass('moveFromRight');
     } else {
-       $('.card-1').addRemove('moveFromLeft');
-      $('.card-2').addRemove('moveFromBottom');
-      $('.card-3').addRemove('moveFromRight');
+      $('.card-1').removeClass('moveFromLeft');
+      $('.card-2').removeClass('moveFromBottom');
+      $('.card-3').removeClass('moveFromRight');
     }
   });
-
-
-//end
 });
 
 
